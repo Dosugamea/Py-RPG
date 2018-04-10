3eaeff010e7d30c079f537e0563783da5ce14022
どまおー <17107514+Dosugamea@users.noreply.github.com>
2018-04-10 18:33:17 +0900

    Rewrite quest

--- rpg_menu.py
+++ rpg_menu.py
@@ -1,4 +1,8 @@
-﻿class Message(object):
+﻿import json,random
+from collections import OrderedDict
+from datetime import datetime
+
+class Message(object):
     def __init__(self,_from="User",to="User",toType=0,text=""):
         self._from = _from
         self.to = to
@@ -56,13 +60,36 @@ class RPG(Logger,Choicer):
                 "Screen":"menu",
                 "Menu":{
                     "MenuID":0,
+                    "Quest_MID":0,
+                    "Setting_ID":0,
                     "Selecting":False
                 },
-                "Quest":{
+                "Quest":{},
+                "Shop":{
+                    "ShopID":0,
+                    "StepID":0,
+                    "Selecting":False,
+                    "Current_Shop":{},
+                    "ShopName":{
+                        "Bought":{
+                            "0":0,
+                            "1":0
+                        },
+                        "Sold":{
+                            "0":0,
+                            "1":0
+                        }
+                    }
                 }
             }
         }
     }
+    
+    with open("ShopData.json",encoding="utf-8_sig") as f:
+        shopdata = json.loads(f.read(), object_pairs_hook=OrderedDict)
+        
+    with open("ItemData.json",encoding="utf-8_sig") as f:
+        itemdata = json.loads(f.read(), object_pairs_hook=OrderedDict)
     
     quest_sel_dict = {
         0:["Main","Sub","Event","Modoru"],
@@ -70,6 +97,8 @@ class RPG(Logger,Choicer):
         2:["Quest1","Quest2","Quest3","Modoru"],
         3:["Quest1","Quest2","Quest3","Modoru"]
     }
+    
+    yobi = ["月","火","水","木","金","土","日"]
     
     def process_rpg(self,msg):
         if self.rpgdata[msg._from]["Stats"]["Screen"] == "menu": self.process_menu(msg)
@@ -86,9 +115,128 @@ class RPG(Logger,Choicer):
         print(self.rpgdata[msg._from]["Stats"])
         
     def process_shop(self,msg):
-        print("Called Shop")
-        print(self.rpgdata[msg._from]["Stats"])
+        #deepcopyではないため直接呼ばれるが問題はないと思われる？
+        stat = self.rpgdata[msg._from]["Stats"]["Shop"]
+        #表示部
+        if stat["Selecting"] == False:
+            #ショップ未選択_選択直後
+            if stat["StepID"] == 0:
+                if stat["ShopID"] == 0:
+                    self.add_log("[Shop]\nWhere to go?",msg)
+                    choices = list(stat["List"].keys())
+                    choices.append("戻る")
+                    self.add_log(self.combine(self.choice_text(choices)),msg)
+                elif stat["ShopID"] in stat["List"].keys():
+                    stat["Current_Shop"] = stat["List"][stat["ShopID"]]
+                    self.add_log("[ %s ]"%(stat["ShopID"]),msg)
+                    if "Welcome" in stat["Current_Shop"]["Messages"] and "Clerk" in stat["Current_Shop"]["Messages"]:
+                        self.add_log("<%s> : %s"%(stat["Current_Shop"]["Messages"]["Clerk"],stat["Current_Shop"]["Messages"]["Welcome"]),msg)
+                    self.add_log("",msg)
+                    actions = []
+                    if "Talk" in stat["Current_Shop"]: actions.append("会話")
+                    #TODO: 互いのインポート処理をここに書こう
+                    if "Buy" in stat["Current_Shop"] and "Deny" not in stat["Current_Shop"]["Buy"]: actions.append("買う")
+                    if "Sell" in stat["Current_Shop"] and "Deny" not in stat["Current_Shop"]["Sell"]: actions.append("売る")
+                    if len(actions) == 0: raise ValueError
+                    actions.append("戻る")
+                    stat["Current_Shop"]["Actions"] = actions
+                    self.add_log(self.combine(self.choice_text(actions)),msg)
+            #会話
+            elif stat["StepID"] == 1:
+                msi = str(random.choice(list(stat["Current_Shop"]["Talk"].keys())))
+                if "Clerk" in stat["Current_Shop"]["Messages"]:
+                    self.add_log("<%s> : %s"%(stat["Current_Shop"]["Messages"]["Clerk"],stat["Current_Shop"]["Talk"][msi]),msg)
+                else:
+                    self.add_log(stat["Current_Shop"]["Talk"][msi],msg)
+                self.add_log("",msg)
+                self.add_log(self.combine(self.choice_text(stat["Current_Shop"]["Actions"])),msg)
+                stat["StepID"] = 0
+            #"プレイヤーが"買う           
+            elif stat["StepID"] == 2:
+                self.add_log("< 商品リスト >",msg)
+                if "Import_Buy" in stat["Current_Shop"]["Sell"]:
+                    items = stat["Current_Shop"]["Buy"]["Items"]
+                else:
+                    items = stat["Current_Shop"]["Sell"]["Items"]
+                for item in items:
+                    im = self.itemdata[str(item["ID"])]
+                    self.add_log("[%s] %s個 %sコイン"%(im["Name"],item["Cnt"],item["Price"]),msg)
+            #"プレイヤーが"売る
+            elif stat["StepID"] == 3:
+                self.add_log("< 買取リスト >",msg)
+                if "Import_Sell" in stat["Current_Shop"]["Buy"]:
+                    items = stat["Current_Shop"]["Sell"]["Items"]
+                else:
+                    items = stat["Current_Shop"]["Item"]["Items"]
+                for item in items:
+                    im = self.itemdata[str(item["ID"])]
+                    self.add_log("[%s] %s個 %sコイン"%(im["Name"],item["Cnt"],item["Price"]),msg)
+            self.send_log(msg)
+            stat["Selecting"] = True
+        #入力受付部
+        else:
+            #ショップ未選択
+            if stat["ShopID"] == 0:
+                choices = list(stat["List"].keys())
+                choices.append("戻る")
+                choice = self.choicer(msg.text,choices)
+                if choice != None:
+                    stat["Selecting"] = False
+                    if choice == "戻る":
+                        self.rpgdata[msg._from]["Stats"]["Menu"]["MenuID"] = 0
+                        self.rpgdata[msg._from]["Stats"]["Screen"] = "menu"
+                        self.process_menu(msg)
+                    else:
+                        stat["ShopID"] = choice
+                        self.process_shop(msg)
+            else:
+                #ショップ内動作選択
+                if stat["StepID"] == 0:
+                    choice = self.choicer(msg.text,stat["Current_Shop"]["Actions"])
+                    if choice != None:
+                        stat["Selecting"] = False
+                        if choice == "戻る": stat["ShopID"] = 0
+                        elif choice == "会話": stat["StepID"] = 1
+                        elif choice == "買う": stat["StepID"] = 2
+                        elif choice == "売る": stat["StepID"] = 3
+                        self.process_shop(msg)
+                #ショップ内購入選択処理
+                elif stat["StepID"] == 2:
+                    pass
+                #ショップ内販売選択処理
+                elif stat["StepID"] == 3:
+                    pass
+                    
+                    
     
+    #曜日指定、期間指定でショップを作る
+    def gen_global_shop(self):
+        shops = self.shopdata
+        cdate = datetime.now()
+        #ショップ一覧の作成
+        ret_ls = {}
+        #日付が有効なリスト
+        for s in shops:
+            if "Limit" not in shops[s]:
+                ret_ls[s] = shops[s]
+            elif "Day" in shops[s]["Limit"]:
+                if shops[s]["Limit"]["Day"][cdate.weekday()] == 1:
+                    ret_ls[s] = shops[s]
+            elif "Date" in shops[s]["Limit"]:
+                _from = datetime.strptime(shops[s]["Limit"]["Date"][0],'%Y-%m-%d %H:%M:%S')
+                to = datetime.strptime(shops[s]["Limit"]["Date"][1],'%Y-%m-%d %H:%M:%S')
+                if cdate > _from and cdate < to:
+                    ret_ls[s] = shops[s]
+            else:
+                ret_ls[s] = shops[s]
+        self.available_shops = ret_ls
+    
+    #個数制限_グローバル個数制限 Deny 等に基づき 個々のユーザー用のショップリストを作る
+    def gen_private_shop(self,msg):
+        shops = self.available_shops
+        return shops
+            
+        
     def process_menu(self,msg):
         stat = self.rpgdata[msg._from]["Stats"]["Menu"]
         if stat["Selecting"] == False:
@@ -110,8 +258,9 @@ class RPG(Logger,Choicer):
                 self.add_log("[Work]\nWhich Work to go?",msg)
                 self.add_log(self.combine(self.choice_text(["看板もち","交通量調査","警備員","パン工場","窓そうじ","工事現場","Modoru"])),msg)
             elif stat["MenuID"] == 3:
-                self.add_log("[Shop]\nWhich Shop to go?",msg)
-                self.add_log(self.combine(self.choice_text(["A","B","C","Modoru"])),msg)
+                self.gen_global_shop()
+                self.rpgdata[msg._from]["Stats"]["Shop"]["List"] = self.gen_private_shop(msg)
+                self.rpgdata[msg._from]["Stats"]["Screen"] = "shop"
             elif stat["MenuID"] == 4:
                 if stat["Setting_ID"] == 0:
                     self.add_log("[Setting]\nWhich Setting to ?",msg)
@@ -121,8 +270,11 @@ class RPG(Logger,Choicer):
                     self.add_log(self.combine(self.choice_text(self.rpgdata[msg._from]["Inventory"].keys())),msg)
                     self.add_log("<Storage>",msg)
                     self.add_log(self.combine(self.choice_text(self.rpgdata[msg._from]["Items"].keys())),msg)
-            self.send_log(msg)
-            self.rpgdata[msg._from]["Stats"]["Menu"]["Selecting"] = True
+            if stat["MenuID"] != 3:
+                self.send_log(msg)
+                self.rpgdata[msg._from]["Stats"]["Menu"]["Selecting"] = True
+            else:
+                self.process_shop(msg)
         else:
             #選択肢に応じて処理―
             #メインメニュー
@@ -174,14 +326,15 @@ class RPG(Logger,Choicer):
                     self.process_menu(msg)
             #ショップメニュー
             elif stat["MenuID"] == 3:
-                choice = self.choicer(msg.text,["A","B","C","Modoru"])
-                if choice != None:
-                    if choice == "":
-                        pass
-                    elif choice == "Modoru":
-                        stat["MenuID"] = 0
-                    self.rpgdata[msg._from]["Stats"]["Menu"]["Selecting"] = False
-                    self.process_menu(msg)
+                pass
+                # choice = self.choicer(msg.text,["A","B","C","Modoru"])
+                # if choice != None:
+                    # if choice == "":
+                        # pass
+                    # elif choice == "Modoru":
+                        # stat["MenuID"] = 0
+                    # self.rpgdata[msg._from]["Stats"]["Menu"]["Selecting"] = False
+                    # self.process_menu(msg)
             #設定メニュー
             elif stat["MenuID"] == 4:
                 choice = self.choicer(msg.text,["Inventory","Equipment","Skill","Use_Announce","Use_Kana","Modoru"])
---
+++ rpg_quest.py
@@ -0,0 +1,360 @@
+﻿import json,random
+from collections import OrderedDict
+from datetime import datetime
+
+'''
+    クエスト名:
+    [X]で固定値 [X,Y]で X～Y表記
+
+    開始要求アイテム Req
+    　　[アイテムID,個数]
+
+    　ウェーブ数:
+    Mapsをlen()してもわからんのでここで指定
+    [X]で固定値 [X,Y]で X～Y表記
+
+    　マップ(ルーム)一覧:
+    {イベントデータ},
+    {イベントデータ or None}
+
+    モンスター(編成一覧):
+    (編成ID)[
+        (ウェーブ)
+        [
+            (チーム)
+            [
+                {"敵データ"},
+                {"敵データ"},
+                {"敵データ"}
+            ]
+        ]
+    ]
+
+    ドロップアイテム
+    　アイテムID 個数 入手確率
+
+    イベントID:
+    1: テキスト表示 Param なし
+    2: ダメージ Param["最低値","最大値"]
+    3: 回復 Param["最低値","最大値"]
+    4: 宝箱 Param[[ドロップアイテムリスト],本物である確率]
+    5: 戦闘 Param[編成ID,編成Wave2,編成Wave3...]
+    6: ジャンプ Param[ジャンプ先ID,ランダムジャンプ先ID,ランダムジャンプ先ID...]
+    7: 分かれ道 Param[[選択肢名,ジャンプ先ID],[選択肢名,ジャンプ先ID]...]
+    8: 行き止まり Param[ドロップアイテムデータ,ドロップアイテムデータ...]
+    9: ゴール Param[ドロップアイテムデータ,ドロップアイテムデータ...]
+
+    コマンド
+    rest
+    go
+    item
+    leave
+    map
+'''
+#てんぷれっ!
+class Message(object):
+    def __init__(self,_from="User",to="User",toType=0,text=""):
+        self._from = _from
+        self.to = to
+        self.toType = 0
+        self.text = text
+class Client(object):
+    def __init__(self):
+        self.reqId = 0
+    def sendMessage(self,text):
+        print("Req<%s>\n\n%s"%(self.reqId,text))
+        self.reqId += 1
+class Logger(object):
+    def add_log(self,text,msg):
+        self.rpgdata[msg._from]["Stats"]["Log"].append(text)
+    def send_log(self,msg):
+        text = self.combine(self.rpgdata[msg._from]["Stats"]["Log"])
+        self.cl.sendMessage(text)
+        self.rpgdata[msg._from]["Stats"]["Log"] = []
+class Choicer(object):
+    choice_dict = {
+        1:"あ",2:"い",3:"う",4:"え",5:"お",
+        6:"か",7:"き",8:"く",9:"け",10:"こ"
+    }
+    def choicer(self,text="あ",ls=["A","B","C"]):
+        if text in ls: return text
+        try:
+            inp = int(text)
+            if inp in self.choice_dict:
+                return ls[inp-1]
+        except:
+            if text in self.choice_dict.values():
+                for c in self.choice_dict:
+                    if self.choice_dict[c] == text:
+                        return ls[c-1]
+    def choice_text(self,ls,kana=False):
+        if kana: return [ " %s : %s"%(self.choice_dict[i+1],l) for i,l in enumerate(ls)]
+        else: return [ " %s : %s"%(i+1,l) for i,l in enumerate(ls)]
+    def combine(self,ls):
+        text = ""
+        for l in ls:
+            text += l+"\n"
+        text = text[:len(text)-1]
+        return text
+
+#クエスト進行 ユーティリティ
+class Q_Utility(object):
+    #使わないだろうけど...
+    def ls2txt(self,ls):
+        if len(ls) == 1: return str(ls[0])
+        else: return "%s～%s"%(ls[0],ls[1])
+    
+    #仮置き。 Battleのを引っ張ってくると動かないからTODO
+    def game_check(self,msg):
+        pass
+    
+    #仮置き。 BattleのNewEntityを引っ張ってくる。
+    def new_entity(self,type,msg):
+        p = {
+            "Name":"Domao",
+            "HP":30,
+            "MAX_HP":30,
+            "MP":20,
+            "MAX_MP":20,
+        }
+        return p
+
+    def new_quest(self,msg):
+        stat = self.rpgdata[msg._from]["Stats"]["Quest"]
+        stat["Current_Quest"] = self.questdata[stat["Quest_Name"]]
+        stat["Current_Position"] = 0
+        stat["Player"] = self.new_entity(1,"MID")
+        self.rpgdata[msg._from]["Stamina"] -= stat["Current_Quest"]["Stamina"]
+        self.add_log("<<< %s に出発します >>>\n"%(stat["Quest_Name"]),msg)
+        self.log_q_status(msg)
+        self.send_log(msg)
+        
+    def log_q_status(self,msg):
+        stat = self.rpgdata[msg._from]["Stats"]["Quest"]
+        self.add_log("",msg)
+        self.add_log("[フロア%s  HP: %s/%s MP: %s/%s]"%(stat["Current_Position"]+1,stat["Player"]["HP"],stat["Player"]["MAX_HP"],stat["Player"]["MP"],stat["Player"]["MAX_MP"]),msg)
+        self.add_log("%sはどうする?"%(self.rpgdata[msg._from]["Name"]),msg)
+        self.add_log(' あ : 進む\n い : 休む\n う : アイテム\n え : マップ\n お : 撤退',msg)
+        
+        
+#クエスト進行 処理本体
+class Q_Process(object):
+    #コマンド:進む
+    def process_go(self,msg):
+        stat = self.rpgdata[msg._from]["Stats"]["Quest"]
+        data = stat["Current_Quest"]["Map"][stat["Current_Position"]]
+        if "Message" in data: self.add_log(data["Message"],msg)
+        if data["Type"] == 2: self.Damage(data,msg)
+        elif data["Type"] == 3: self.Heal(data,msg)
+        elif data["Type"] == 4: self.Treasure(data,msg)
+        elif data["Type"] == 5: self.Battle(data,msg)
+        elif data["Type"] == 6: self.Jump(data,msg)
+        elif data["Type"] == 7: self.Switch(data,msg)
+        elif data["Type"] == 8: self.Stop(data,msg)
+        elif data["Type"] == 9: self.Goal(data,msg)
+        stat["Current_Position"] += 1
+        self.log_q_status(msg)
+        self.send_log(msg)
+    
+    #コマンド:休む
+    def process_rest(self,msg):
+        pass
+        
+    def Damage(self,data,msg):
+        stat = self.rpgdata[msg._from]["Stats"]["Quest"]
+        '''
+        2: ダメージ Param["最低値","最大値"]
+        '''
+        dmg = data["Param"]
+        if len(dmg) == 1:
+            if isinstance(dmg[0], str):
+                dmg = int(stat["Player"]["HP"] * (int(dmg[0][:dmg[0].find("%")])/100))
+            else:
+                dmg = dmg[0]
+        else:
+            if isinstance(dmg[0], str): st = int(stat["Player"]["HP"] * (int(dmg[0][:dmg[0].find("%")])/100))
+            else: st = dmg[0]
+            if isinstance(dmg[1], str): ed = int(stat["Player"]["HP"] * (int(dmg[1][:dmg[1].find("%")])/100))
+            else: ed = dmg[1]
+            dmg = random.randint(st,ed)
+        stat["Player"]["HP"] -= dmg
+        self.add_log("%sは%sのダメージを受けた"%(stat["Player"]["Name"],dmg),msg)
+        if stat["Player"]["HP"] <= 0:
+            self.add_log("%sは倒れた"%(stat["Player"]["Name"]),msg)
+            self.game_check(msg)
+        
+    def Heal(self,data,msg):
+        '''
+        3: 回復 Param["最低値","最大値"]
+        '''
+        hel = data["Param"]
+        if len(hel) == 1:
+            if isinstance(hel[0], str): hel = int(self.user.MAX_HP * (int(hel[0][:hel[0].find("%")])/100))
+        elif len(hel) == 2:
+            hel = []
+            if isinstance(hel[0], str): hel[0] = int(self.user.MAX_HP * (int(hel[0][:hel[0].find("%")])/100))
+            if isinstance(hel[1], str): hel[1] = int(self.user.MAX_HP * (int(hel[1][:hel[1].find("%")])/100))
+            hel = random.randint(hel[0],hel[1])
+        self.user.HP += hel
+        print("%sは%s 回復した"%(self.user.name,hel))
+        if self.user.HP > self.user.MAX_HP: self.user.HP = self.user.MAX_HP
+        
+    def Get_Item(self,itemlist,msg):
+        '''
+        4 8 9: アイテム入手 Param[[[アイテムID,個数,入手確率],[アイテムID,個数,入手確率]...]]
+        '''
+        print("引数: %s"%(itemlist))
+        
+    def Treasure(self,data,msg):
+        '''
+        4: 宝箱 Param[[ドロップアイテムリスト],本物である確率,[偽物である場合の編成リスト]]
+        '''
+        while True:
+            inp = input("開けますか? >>")
+            if inp == "y":
+                #ドロップアイテムリストだけなら リストをGetに投げる
+                if len(data["Param"]) == 1:
+                    self.Get_Item(data["Param"][0])
+                elif len(data["Param"]) == 2:
+                    per = random.randint(1,100)
+                    if per <= data["Param"][1]:
+                        self.Get_Item(data["Param"][0])
+                    else:
+                        print("中身は空だった...")
+                else:
+                    per = random.randint(1,100)
+                    if per <= data["Param"][1]:
+                        self.Get_Item(data["Param"][0])
+                    else:
+                        print("!? これは宝箱じゃないぞ!")
+                        self.Battle(random.choice(self.data["Param"][2]))
+                break
+            elif inp == "n":
+                print("罠かもしれない。 開けないことにした...")
+                break
+        
+    def Battle(self,data,msg):
+        '''
+        5: 戦闘 Param[編成ID,編成Wave2,編成Wave3...]
+        '''
+        print("引数: %s"%(data))
+    
+    def Jump(self,data,msg):
+        '''
+        6: ジャンプ Param[ジャンプ先ID,ランダムジャンプ先ID,ランダムジャンプ先ID...]
+        '''
+        self.cp = random.choice(data["Param"]) -1
+    
+    def Switch(self,data,msg):
+        '''
+        7: 分かれ道 Param[[選択肢名,ジャンプ先ID],[選択肢名,ジャンプ先ID]...]
+        '''
+        jumped = False
+        for choice in data["Param"]:
+            print("->%s"%(choice[0]))
+        while True:
+            if jumped: break
+            inp = input('どこに進む? >>')
+            for choice in data["Param"]:
+                if inp == choice[0]:
+                    self.cp = choice[1]
+                    self.GO()
+                    jumped = True
+                    break
+
+    def Stop(self,data,msg):
+        '''
+        8: 行き止まり Param[ドロップアイテムデータ,ドロップアイテムデータ...]
+        '''
+        #強制終了フラグを立てる
+        self.cp = -2
+        if "Param" in data: self.Get_Item(data["Param"])
+        
+    def Goal(self,data,msg):
+        '''
+        9: ゴール Param[ドロップアイテムデータ,ドロップアイテムデータ...]
+        '''
+        self.cp = -1
+        if "Param" in data: self.Get_Item(data["Param"])
+    
+
+
+class RPG(Logger,Choicer,Q_Process,Q_Utility):
+    cl = Client()
+    rpgdata = {
+        "User":{
+            "Name":"Domao",
+            "Stamina": 20,
+            "Stone": 100,
+            "Money": 100,
+            "Inventory":{},
+            "Items":{},
+            "Stats":{
+                "Log":[],
+                "Screen":"menu",
+                "Menu":{
+                    "MenuID":0,
+                    "Quest_MID":0,
+                    "Setting_ID":0,
+                    "Selecting":False
+                },
+                "Quest":{
+                    "Quest_Name":"デバッグクエスト",
+                    "Current_Position":0,
+                    "Current_Quest":{},
+                    "Questing": True
+                },
+                "Shop":{
+                    "ShopID":0,
+                    "StepID":0,
+                    "Selecting":False,
+                    "Current_Shop":{},
+                    "ShopName":{
+                        "Bought":{
+                            "0":0,
+                            "1":0
+                        },
+                        "Sold":{
+                            "0":0,
+                            "1":0
+                        }
+                    }
+                }
+            }
+        }
+    }
+    with open("QuestData.json",encoding="utf-8_sig") as f:
+        questdata = json.loads(f.read(), object_pairs_hook=OrderedDict)
+        
+    def process_rpg(self,msg):
+        stat = self.rpgdata[msg._from]["Stats"]
+        if stat["Quest"]["Questing"]: self.process_quest(msg)
+    
+    def process_quest(self,msg):
+        stat = self.rpgdata[msg._from]["Stats"]["Quest"]
+        choice = self.choicer(msg.text,["進む","休む","アイテム","マップ","撤退","はい","いいえ"])
+        if choice == "進む":
+            self.process_go(msg)
+        elif choice == "休む":
+            self.process_rest(msg)
+        elif choice == "アイテム":
+            pass
+        elif choice == "マップ":
+            #画像を送るかMap -> Text 関数を使うか
+            pass
+        elif choice == "撤退":
+            self.add_log("撤退するとクエスト失敗になりますが\nよろしいですか?",msg)
+            self.add_log(" か: はい\n き: いいえ",msg)
+            self.send_log(msg)
+        elif choice == "はい":
+            pass
+        elif choice == "いいえ":
+            pass
+
+
+RPGer = RPG()
+RPGer.new_quest(Message())
+
+while True:
+    inp = input(">>")
+    RPGer.process_rpg(Message(text=inp))
